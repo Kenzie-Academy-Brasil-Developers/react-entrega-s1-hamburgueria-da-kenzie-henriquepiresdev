ad570311e66568097606ae675a886c1c487f8686
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2022-07-28 11:44:52 -0300

    main: inicializando Components, importando fonts e cores

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import GlobalStyle from "./styles/Global";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/Global.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+:root{
+  --color-primary:#27AE60;
+  --color-primary-50:#93D7AF;
+  --color-secondary:#EB5757;
+  --color-gray-100: #333333;
+  --color-grey-50:#828282;
+  --color-grey-20:#E0E0E0;
+  --color-grey-0:#f5f5f5;
+  --color-negative: #e60000;
+  --color-warning: #ffcd07;
+  --color-sucess: #168821;
+  --color-information: #155bcb;
+  
+}
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Inter', sans-serif;
+}
+
+
+`;
+export default GlobalStyle;
