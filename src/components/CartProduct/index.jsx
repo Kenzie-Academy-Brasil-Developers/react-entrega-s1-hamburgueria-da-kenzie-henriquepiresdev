f0df2759e1b9597e330bfdbd123167cc9af9264e
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2022-07-29 11:05:41 -0300

    feat:finalizando a estilização

---
+++ src/components/CartProduct/index.jsx
@@ -0,0 +1,17 @@
+import { DivCart } from "./style";
+
+function CartProduct() {
+  return (
+    <DivCart>
+      <figure>
+        <img src="" alt="" />
+      </figure>
+      <div>
+        <h4> Hamburguer</h4>
+        <p>Sanduíches</p>
+        <button>remover</button>
+      </div>
+    </DivCart>
+  );
+}
+export default CartProduct;
